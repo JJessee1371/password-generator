3fdf002dbbe6a3ed73c33eaf438c9d9648cd8147
Jon Jessee <tjessee7624@gmail.com>
2020-08-01 20:24:14 -0600

    Created prompts and confirms to log user input for password requirements.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -12,3 +12,27 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//User prompt questions: 
+
+//String length between 8 and 128
+
+let length = prompt("How many characters long would you like your password?");
+  console.log(length);
+
+
+//Lowercase 
+let lowerCase = confirm("Would you like to include lower case letters?");
+console.log(lowerCase);
+
+//Uppercase
+let upperCase = confirm("Would you like to include upper case letters?");
+console.log(upperCase);
+
+//Numeric
+let num = confirm("Would you like to include numbers?");
+console.log(num);
+
+//Special characters
+let special = confirm("Would you like to include special characters?");
+console.log(special);
---
+++ pseudo.md
@@ -0,0 +1,21 @@
+Start point: User clicks on a button, "genreate password button"
+
+Collect user input for password criteria. 
+    -Length
+        -Min 8 and max 128 characters
+    -Specail characters
+        -Which should be included?
+    -Numbers
+    -Upper and lowercase letters
+
+Randomize the input and order it. 
+
+Select random characters in each category 
+    -random number
+    -random letters
+    -random symbols
+
+display password on screen
+
+generate password is not defined in the JS file, so that needs to be declared. function generatePassword() { }
+console.log("generatePassword is running"). This will change the text box from placeholder to an undefined. There is a variable being displayed that does not yet have value.  
