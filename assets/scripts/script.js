bb34fe8bda78cd44b2ae55b85cef89b49f4fea51
unknown <tjessee7624@gmail.com>
2021-12-16 14:40:22 -0500

    Each selected character group is now guranteed at least one character for added security

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,65 +1,65 @@
-//Varaible declarations
-let text = document.querySelector('#password');
-const lenSelect = document.getElementById('length');
-const capital = document.getElementById('capital');
-const lower = document.getElementById('lower');
-const nums = document.getElementById('numbers');
-const special = document.getElementById('special');
-let combinedArr = [];
+let resultText = document.querySelector('#password');
+const lengthSelect = document.querySelector('#length');
+const capital = document.querySelector('#capital');
+const lower = document.querySelector('#lower');
+const nums = document.querySelector('#numbers');
+const special = document.querySelector('#special');
+let resultArr = [];
 const upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const lowerLetters = [];
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "~", "<", ">"];
 
-//Form submission event listener
-document.getElementById('create-form').addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  let password = generatePassword();
-  text.value = password;
-});
-
 //Convert upperLetters array to lower case letters
 for(i = 0; i < upperLetters.length; i++) {
   let letter = upperLetters[i].toLowerCase();
   lowerLetters.push(letter);
-}
+};
+
+//Form submission event listener
+document.querySelector('#create-form').addEventListener("submit", (event) => {
+  event.preventDefault();
+  let password = generatePassword();
+  resultText.value = password;
+});
+
 
 //Generates password based on the user input if a length and character type have been selected
 function generatePassword() {
-  if(!lenSelect.value || !capital.checked && !lower.checked && !nums.checked && !special.checked) {
-    alert('You must select a length and a minimum of 1 character type!');
+  let password = [];
+  let count = 0;
+  if(!lengthSelect.value || !capital.checked && !lower.checked && !nums.checked && !special.checked) {
+    alert('A password length and at least one character type must be selected.');
+    return;
+  } else if(lengthSelect.value < 8 || lengthSelect.value > 128) {
+    alert('The password length must be between 8 and 128 characters.');
     return;
   } else {
     if(capital.checked) {
-      combinedArr = combinedArr.concat(upperLetters);
-    }
-  
-    if(lower.checked) {
-      combinedArr = combinedArr.concat(lowerLetters);
-    }
-  
-    if(nums.checked) {
-      combinedArr = combinedArr.concat(numbers);
-    }
-  
-    if(special.checked) {
-      combinedArr = combinedArr.concat(specChar);
-    }
-  
-    let password = '';
-  
-    for (i = 0; i < lenSelect.value; i++) {
-        password = password + combinedArr[Math.floor(Math.random() * combinedArr.length)];
+      resultArr = resultArr.concat(upperLetters);
+      password.push(upperLetters[Math.floor(Math.random() * upperLetters.length)]);
+      count++;
     };
-    return password;
-  }
+    if(lower.checked) {
+      resultArr = resultArr.concat(lowerLetters);
+      password.push(lowerLetters[Math.floor(Math.random() * lowerLetters.length)]);
+      count++;
+    };
+    if(nums.checked) {
+      resultArr = resultArr.concat(numbers);
+      password.push(numbers[Math.floor(Math.random() * numbers.length)]);
+      count++;
+    };
+    if(special.checked) {
+      resultArr = resultArr.concat(specChar);
+      password.push(specChar[Math.floor(Math.random() * specChar.length)]);
+      count++;
+    };
+    
+    for(i = 0; i < lengthSelect.value - count; i++) {
+      password.push(resultArr[Math.floor(Math.random() * resultArr.length)]);
+    };
+
+    return password.join('');
+  };
 };
-
-
-
-
-
-
-
-
